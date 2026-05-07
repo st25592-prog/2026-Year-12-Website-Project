1ed635682110613921f1621548f062b31060065e
Emarina <st25592@ormiston.school.nz>
2026-05-08 10:33:19 +1200

    Added JSON flower file and coding

--- CSC2_Flask_Practise/app.py
+++ CSC2_Flask_Practise/app.py
@@ -1,11 +1,30 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
+import json
+from flask import Flask, render_template, request, redirect, url_for, session, flash 
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'
 
-if __name__ == '__main__':
-    app.run(debug=True)
+def load_data():
+    with open('data/flowers.json') as file:
+        data = json.load(file)
+    return data
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    flowers = load_data()
+    return render_template('index.html', flowers=flowers)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/checkout')
+def checkout():
+    return render_template('invoice.html')
+
+@app.route('/orders')
+def order_history():
+    return render_template('order_history.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
